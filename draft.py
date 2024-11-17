d05eb3735829d7ece117740ccbf17e0303d9799e
Lucidreline <casta.ma502@gmail.com>
2024-11-17 06:14:44 +0000

    Added variables to the pin number.

--- draft.py
+++ draft.py
@@ -1,17 +1,26 @@
 import Adafruit_DHT as dht
-from time import sleep
+import sys
+
 #Set DATA pin
-DHT = 22
+pinNumber = int(sys.argv[1]) # 11 or 22
+print(sys.argv[1])
 
 def celToFer(c):
     return (c * (9/5)) + 32
+print('test')
 
-while True:
-    #Read Temp and Hum from DHT22
-    humidity,temperature = dht.read_retry(dht.DHT22, DHT)
-    temperature = celToFer(temperature)
-    #Print Temperature and Humidity on Shell window
-    print('Temp={0:0.1f}F  Humidity={1:0.1f}%'.format(temperature,humidity))
-    sleep(5) #Wait 5 seconds and read again
+try:
+    if (pinNumber == 11):
+        humidity,temperature = dht.read_retry(dht.DHT22, pinNumber)
+    elif (pinNumber == 22):
+        humidity,temperature = dht.read_retry(dht.DHT22, pinNumber)
+except:
+    print("oopsie")
+
+
+
+temperature = celToFer(temperature)
+#Print Temperature and Humidity on Shell window
+print('Temp={0:0.1f}F  Humidity={1:0.1f}%'.format(temperature,humidity))
 
 
---
+++ t.py
@@ -0,0 +1,1 @@
+print("hey am I cronnend?")

205d50a90841d9c0739f40585b6a5d5c6b987a61
Lucidreline <casta.ma502@gmail.com>
2024-11-17 16:56:37 -0800

    Cloud lambda function now correctly saved data to the DB.

--- sam-app/.aws-sam/build/getByIdFunction/src/handlers/put-item.mjs
+++ sam-app/.aws-sam/build/getByIdFunction/src/handlers/put-item.mjs
@@ -29,14 +29,12 @@ export const putItemHandler = async (event) => {
 
     // Get id and name from the body of the request
     const body = JSON.parse(event.body);
-    const id = body.id;
-    const name = body.name;
 
     // Creates a new item, or replaces an old item with a new item
     // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
     var params = {
         TableName: tableName,
-        Item: { id: id, name: name }
+        Item: { ...body }
     };
 
     try {
